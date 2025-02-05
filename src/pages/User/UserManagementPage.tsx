09e40bc5814e1a25515c8ec629f1e14c665e12a1
Helmi <helmi220602@gmail.com>
2025-02-05 21:19:09 +0800

    update dashboard user management logic desc&asc

--- src/pages/User/UserManagementPage.tsx
+++ src/pages/User/UserManagementPage.tsx
@@ -2,9 +2,24 @@ import { Divider } from "@mui/material";
 import SideNav from "../../components/SideNav";
 import Box from "@mui/material/Box";
 import { IoSearch } from "react-icons/io5";
+import { RxDoubleArrowUp } from "react-icons/rx";
+import { RxDoubleArrowDown } from "react-icons/rx";
+import { useState } from "react";
 
 const UserManagementPage = () => {
-  const userList = [
+  // Definisikan tipe data User
+  type User = {
+    id: number;
+    npk: string;
+    name: string;
+    email: string;
+    roles: string;
+    job: string;
+    department: string;
+    superior: string;
+  };
+
+  const userList: User[] = [
     {
       id: 24,
       npk: "11211043",
@@ -56,6 +71,26 @@ const UserManagementPage = () => {
       superior: "Rasyid",
     },
   ];
+
+  const [sortField, setSortField] = useState<keyof User | null>(null);
+  const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc");
+
+  const handleSort = (field: keyof User) => {
+    if (sortField === field) {
+      setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+    } else {
+      setSortField(field);
+      setSortOrder("asc");
+    }
+  };
+
+  const sortedUserList = [...userList].sort((a, b) => {
+    if (!sortField) return 0;
+    if (a[sortField] < b[sortField]) return sortOrder === "asc" ? -1 : 1;
+    if (a[sortField] > b[sortField]) return sortOrder === "asc" ? 1 : -1;
+    return 0;
+  });
+
   return (
     <Box sx={{ display: "flex" }}>
       <SideNav />
@@ -86,67 +121,233 @@ const UserManagementPage = () => {
 
         {/* table user management by daisyUI */}
         <div className="overflow-x-auto mt-4">
-          <table className="table">
+          <table className="table table-zebra">
             {/* head */}
             <thead>
-              <tr>
+              <tr className="bg-gray-50">
+                <th></th>
+                {/* ID */}
                 <th>
-                  <label>
-                    <input type="checkbox" className="checkbox" />
-                  </label>
+                  <div className="flex items-center gap-1">
+                    <p className="text-black text-sm">ID</p>
+                    <div
+                      onClick={() => handleSort("id")}
+                      className="cursor-pointer"
+                    >
+                      {sortField === "id" ? (
+                        sortOrder === "asc" ? (
+                          <RxDoubleArrowUp className="text-black w-4 h-4" />
+                        ) : (
+                          <RxDoubleArrowDown className="text-black w-4 h-4" />
+                        )
+                      ) : (
+                        <div className="flex flex-row">
+                          <RxDoubleArrowUp className="text-slate-500" />
+                          <RxDoubleArrowDown className="text-slate-500" />
+                        </div>
+                      )}
+                    </div>
+                  </div>
                 </th>
-                <th>ID</th>
-                <th>NPK</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Roles</th>
-                <th>Job</th>
-                <th>Department</th>
-                <th>Superior</th>
-                {/* <th></th> */}
+                {/* NPK */}
+                <th>
+                  <div className="flex items-center gap-1">
+                    <p className="text-black text-sm">NPK</p>
+                    <div
+                      onClick={() => handleSort("npk")}
+                      className="cursor-pointer"
+                    >
+                      {sortField === "npk" ? (
+                        sortOrder === "asc" ? (
+                          <RxDoubleArrowUp className="text-black w-4 h-4" />
+                        ) : (
+                          <RxDoubleArrowDown className="text-black w-4 h-4" />
+                        )
+                      ) : (
+                        <div className="flex flex-row">
+                          <RxDoubleArrowUp className="text-slate-500" />
+                          <RxDoubleArrowDown className="text-slate-500" />
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                </th>
+                {/* Name */}
+                <th>
+                  <div className="flex items-center gap-1">
+                    <p className="text-black text-sm">Name</p>
+                    <div
+                      onClick={() => handleSort("name")}
+                      className="cursor-pointer"
+                    >
+                      {sortField === "name" ? (
+                        sortOrder === "asc" ? (
+                          <RxDoubleArrowUp className="text-black w-4 h-4" />
+                        ) : (
+                          <RxDoubleArrowDown className="text-black w-4 h-4" />
+                        )
+                      ) : (
+                        <div className="flex flex-row">
+                          <RxDoubleArrowUp className="text-slate-500" />
+                          <RxDoubleArrowDown className="text-slate-500" />
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                </th>
+                {/* Email */}
+                <th>
+                  <div className="flex items-center gap-1">
+                    <p className="text-black text-sm">Email</p>
+                    <div
+                      onClick={() => handleSort("name")}
+                      className="cursor-pointer"
+                    >
+                      {sortField === "email" ? (
+                        sortOrder === "asc" ? (
+                          <RxDoubleArrowUp className="text-black w-4 h-4" />
+                        ) : (
+                          <RxDoubleArrowDown className="text-black w-4 h-4" />
+                        )
+                      ) : (
+                        <div className="flex flex-row">
+                          <RxDoubleArrowUp className="text-slate-500" />
+                          <RxDoubleArrowDown className="text-slate-500" />
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                </th>
+                {/* Roles */}
+                <th>
+                  <div className="flex items-center gap-1">
+                    <p className="text-black text-sm">Roles</p>
+                    <div
+                      onClick={() => handleSort("roles")}
+                      className="cursor-pointer"
+                    >
+                      {sortField === "roles" ? (
+                        sortOrder === "asc" ? (
+                          <RxDoubleArrowUp className="text-black w-4 h-4" />
+                        ) : (
+                          <RxDoubleArrowDown className="text-black w-4 h-4" />
+                        )
+                      ) : (
+                        <div className="flex flex-row">
+                          <RxDoubleArrowUp className="text-slate-500" />
+                          <RxDoubleArrowDown className="text-slate-500" />
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                </th>
+                {/* Job */}
+                <th>
+                  <div className="flex items-center gap-1">
+                    <p className="text-black text-sm">Job</p>
+                    <div
+                      onClick={() => handleSort("job")}
+                      className="cursor-pointer"
+                    >
+                      {sortField === "job" ? (
+                        sortOrder === "asc" ? (
+                          <RxDoubleArrowUp className="text-black w-4 h-4" />
+                        ) : (
+                          <RxDoubleArrowDown className="text-black w-4 h-4" />
+                        )
+                      ) : (
+                        <div className="flex flex-row">
+                          <RxDoubleArrowUp className="text-slate-500" />
+                          <RxDoubleArrowDown className="text-slate-500" />
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                </th>
+                {/* Department */}
+                <th>
+                  <div className="flex items-center gap-1">
+                    <p className="text-black text-sm">Department</p>
+                    <div
+                      onClick={() => handleSort("department")}
+                      className="cursor-pointer"
+                    >
+                      {sortField === "department" ? (
+                        sortOrder === "asc" ? (
+                          <RxDoubleArrowUp className="text-black w-4 h-4" />
+                        ) : (
+                          <RxDoubleArrowDown className="text-black w-4 h-4" />
+                        )
+                      ) : (
+                        <div className="flex flex-row">
+                          <RxDoubleArrowUp className="text-slate-500" />
+                          <RxDoubleArrowDown className="text-slate-500" />
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                </th>
+                {/* Superior */}
+                <th>
+                  <div className="flex items-center gap-1">
+                    <p className="text-black text-sm">Superior</p>
+                    <div
+                      onClick={() => handleSort("superior")}
+                      className="cursor-pointer"
+                    >
+                      {sortField === "superior" ? (
+                        sortOrder === "asc" ? (
+                          <RxDoubleArrowUp className="text-black w-4 h-4" />
+                        ) : (
+                          <RxDoubleArrowDown className="text-black w-4 h-4" />
+                        )
+                      ) : (
+                        <div className="flex flex-row">
+                          <RxDoubleArrowUp className="text-slate-500" />
+                          <RxDoubleArrowDown className="text-slate-500" />
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                </th>
+                <th></th>
               </tr>
             </thead>
+            {/* isi data */}
             <tbody>
-              {userList.map((itemList, index) => (
+              {sortedUserList.map((itemList, index) => (
                 <tr key={index}>
                   <th>
                     <label>
                       <input type="checkbox" className="checkbox" />
                     </label>
                   </th>
-                  <td>
-                    <div className="text-sm text-black">{itemList.id}</div>
-                  </td>
-                  <td>
-                    <div className="text-sm text-black">{itemList.npk}</div>
-                    {/* <br />
-                  <span className="badge badge-ghost badge-sm">
-                  Desktop Support Technician
-                  </span> */}
-                  </td>
+                  <td className="text-sm text-black">{itemList.id}</td>
+                  <td className="text-sm text-black">{itemList.npk}</td>
                   <td>{itemList.name}</td>
                   <td>{itemList.email}</td>
-                  <td>{itemList.roles}</td>
+                  <td>
+                    <p className="bg-orange-500 py-1 px-2 rounded-md text-white text-center">
+                      {itemList.roles}
+                    </p>
+                  </td>
                   <td>{itemList.job}</td>
                   <td>{itemList.department}</td>
-                  <th>
-                    <button className="btn btn-ghost btn-xs">{itemList.superior}</button>
-                  </th>
-                  <th className="flex flex-col gap-2 ">
-                    <button className="border border-blue-700 p-2 rounded-sm bg-blue-500 text-white">
+                  <td>{itemList.superior}</td>
+                  <td className="flex flex-col gap-2 ">
+                    <button className="border border-blue-700 px-2 py-1 rounded-sm bg-blue-500 text-white capitalize hover:bg-blue-700 duration-150">
                       view
                     </button>
-                    <button className="border border-sky-500 p-2 rounded-sm bg-sky-300 text-white">
+                    <button className="border border-sky-500 px-2 py-1 rounded-sm bg-sky-300 text-white capitalize hover:bg-sky-700 duration-150">
                       edit
                     </button>
-                    <button className="border border-red-700 p-2 rounded-sm bg-red-500 text-white">
+                    <button className="border border-red-700 px-2 py-1 rounded-sm bg-red-500 text-white capitalize hover:bg-red-700 duration-150">
                       delete
                     </button>
-                  </th>
+                  </td>
                 </tr>
               ))}
             </tbody>
-            {/* foot */}
           </table>
         </div>
       </Box>
