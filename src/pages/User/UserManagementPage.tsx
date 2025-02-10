d9129c41a8be1a92560acb9bc0059a4841e1ffe5
Helmi <helmi220602@gmail.com>
2025-02-10 13:38:02 +0800

    update clean code

--- src/pages/User/UserManagementPage.tsx
+++ src/pages/User/UserManagementPage.tsx
@@ -179,7 +179,7 @@ const UserManagementPage = () => {
     });
   };
 
-  const paginationNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  // const paginationNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
   return (
     <Box sx={{ display: "flex" }}>
