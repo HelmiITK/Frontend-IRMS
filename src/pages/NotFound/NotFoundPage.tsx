8a6970f96d11e8c4df64fb2196f4cd9cc7f4a6cc
Helmi <helmi220602@gmail.com>
2025-02-14 15:37:48 +0800

    revisi footer

--- src/pages/NotFound/NotFoundPage.tsx
+++ src/pages/NotFound/NotFoundPage.tsx
@@ -1,6 +1,7 @@
 import { FaArrowLeft } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
 import Bg from "../../assets/bgNotFound.jpg";
+import cortex from "../../assets/logoo_coretax-removebg-preview.png";
 
 const NotFoundPage: React.FC = () => {
   const navigate = useNavigate();
@@ -15,9 +16,14 @@ const NotFoundPage: React.FC = () => {
         <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-80 z-5"></div>
 
         <div className="text-center absolute z-10 top-[40%] flex flex-col justify-center items-center">
-          <h1 className="font-bold text-5xl text-white animate-fadeIn transition-all font-montserrat">
+          <h1 className="font-bold text-6xl text-white animate-fadeIn transition-all font-montserrat">
             {" "}
-            <span className="text-red-500">404</span> | Not Found!
+            {/* <span className="text-red-500">404</span> | Not Found! */}
+            <div className="flex items-center justify-center gap-6">
+              <h1 className="text-green-500">404</h1>
+              <p>|</p>
+              <img src={cortex} alt="" className="w-1/3 shadow-lg bg-white" />
+            </div>
           </h1>
 
           <h2
