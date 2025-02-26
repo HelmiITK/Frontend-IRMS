605f03bf1de22eb31a2878493220a875eb5a0a32
Helmi <helmi220602@gmail.com>
2025-02-26 15:03:28 +0800

    done language table matrix

--- src/components/DashboardComponents/TableMatrixIncidentReportComponent.tsx
+++ src/components/DashboardComponents/TableMatrixIncidentReportComponent.tsx
@@ -1,90 +1,6 @@
 import { useState } from "react";
 import { FiMinus, FiPlus } from "react-icons/fi";
 import { motion } from "framer-motion";
-
-interface incidentMatrix {
-  kategoriIncident: string;
-  manusia: string;
-  asset: string[];
-  lingkungan: string;
-  reputasi: string[];
-  securityFisik: string;
-  securitySiber: string;
-}
-
-const tableMatrix: incidentMatrix[] = [
-  {
-    kategoriIncident: "Insignificant",
-    manusia: "First aid case (tanpa tindakan medis)",
-    asset: ["Pabrik beroperasi normal dengan kondisi gangguan tidak berarti"],
-    lingkungan: "Pencemaran di lingkungan kerja",
-    reputasi: ["Reaksi di internal kawasan industri KIE"],
-    securityFisik: "Masuk tanpa izin, tidak ada kehilangan",
-    securitySiber:
-      "Penggunaan hardware yang tidak terdaftar tanpa kehilangan informasi perusahaan",
-  },
-  {
-    kategoriIncident: "Minor",
-    manusia: "First aid case (ada tindakan medis)",
-    asset: [
-      "Pabrik beroperasi normal dengan gangguan yang menyebabkan perbaikan di tempat",
-    ],
-    lingkungan: "Pencemaran di lingkungan perusahaan",
-    reputasi: ["Liputan oleh media lokal (Bontang Post)"],
-    securityFisik:
-      "Masuk tanpa izin, tidak ada kehilangan, dan mengganggu kenyamanan",
-    securitySiber: "System down kurang dari 2 jam",
-  },
-  {
-    kategoriIncident: "Moderate",
-    manusia: "Medical treatment case",
-    asset: [
-      "Pabrik beroperasi tidak normal dan perlu perbaikan dengan menurunkan rate produksi",
-      "Kerugian < USD 50.000",
-    ],
-    lingkungan: "Pencemaran ke masyarakat",
-    reputasi: [
-      "Liputan oleh media regional (Kaltim Post)",
-      "Aduan kerusakan oleh masyarakat",
-    ],
-    securityFisik: "Masuk tanpa izin, ada kehilangan 1 barang",
-    securitySiber:
-      "System down 2-8 jam, kehilangan data perusahaan secara permanen",
-  },
-  {
-    kategoriIncident: "Major",
-    manusia: "Lost workday case atau cacat permanen",
-    asset: [
-      "Pabrik shutdown dengan kerusakan yang harus diperbaiki sampai dengan 5 hari",
-      "Kerugian USD 50.000 - 100.000",
-    ],
-    lingkungan: "Pencemaran ke masyarakat dan menimbulkan protes masyarakat",
-    reputasi: [
-      "Liputan oleh media Nasional",
-      "Tuntutan hukum oleh masyarakat/LSM",
-      "PROPER Nasional MERAH",
-    ],
-    securityFisik: "Masuk tanpa izin, ada kehilangan lebih dari 1 barang",
-    securitySiber:
-      "System diretas, tanpa diketahui, dan sewaktu-waktu dapat dikendalikan dari luar",
-  },
-  {
-    kategoriIncident: "Catastrophic",
-    manusia: "Kematian (1 atau lebih orang)",
-    asset: [
-      "Pabrik shutdown dengan memerlukan perbaikan lebih dari 5 hari",
-      "Kerugian > USD 100.000",
-    ],
-    lingkungan: "Pencemaran lingkungan yang menimbulkan tuntutan hukum",
-    reputasi: [
-      "Liputan oleh media Internasional",
-      "Investigasi dari Pemerintah/Kepolisian",
-    ],
-    securityFisik: "Ancaman bom, pembajakan pabrik, sabotase",
-    securitySiber: "Kelumpuhan infrastruktur IT",
-  },
-];
-
 interface TableMatrixIncidentReportComponentProps {
   t: (key: string) => string;
 }
@@ -92,13 +8,90 @@ interface TableMatrixIncidentReportComponentProps {
 const TableMatrixIncidentReportComponent: React.FC<
   TableMatrixIncidentReportComponentProps
 > = ({ t }) => {
+  interface incidentMatrix {
+    kategoriIncident: string;
+    manusia: string;
+    asset: string[];
+    lingkungan: string;
+    reputasi: string[];
+    securityFisik: string;
+    securitySiber: string;
+  }
+
+  const tableMatrix: incidentMatrix[] = [
+    {
+      kategoriIncident: "Insignificant",
+      manusia: `${t(`table.fieldTable.Insignificant.manusia`)}`,
+      asset: [`${t(`table.fieldTable.Insignificant.asset`)}`],
+      lingkungan: `${t(`table.fieldTable.Insignificant.lingkungan`)}`,
+      reputasi: [`${t(`table.fieldTable.Insignificant.reputasi`)}`],
+      securityFisik: `${t(`table.fieldTable.Insignificant.securityFisik`)}`,
+      securitySiber: `${t(`table.fieldTable.Insignificant.securitySiber`)}`,
+    },
+    {
+      kategoriIncident: "Minor",
+      manusia: `${t(`table.fieldTable.minor.manusia`)}`,
+      asset: [`${t(`table.fieldTable.minor.asset`)}`],
+      lingkungan: `${t(`table.fieldTable.minor.lingkungan`)}`,
+      reputasi: [`${t(`table.fieldTable.minor.reputasi`)}`],
+      securityFisik: `${t(`table.fieldTable.minor.securityFisik`)}`,
+      securitySiber: `${t(`table.fieldTable.minor.securitySiber`)}`,
+    },
+    {
+      kategoriIncident: "Moderate",
+      manusia: `${t(`table.fieldTable.moderate.manusia`)}`,
+      asset: [
+        `${t(`table.fieldTable.moderate.asset.a1`)}`,
+        `${t(`table.fieldTable.moderate.asset.a2`)}`,
+      ],
+      lingkungan: `${t(`table.fieldTable.moderate.lingkungan`)}`,
+      reputasi: [
+        `${t(`table.fieldTable.moderate.reputasi.r1`)}`,
+        `${t(`table.fieldTable.moderate.reputasi.r2`)}`,
+      ],
+      securityFisik: `${t(`table.fieldTable.moderate.securityFisik`)}`,
+      securitySiber: `${t(`table.fieldTable.moderate.securitySiber`)}`,
+    },
+    {
+      kategoriIncident: "Major",
+      manusia: `${t(`table.fieldTable.major.manusia`)}`,
+      asset: [
+        `${t(`table.fieldTable.major.asset.a1`)}`,
+        `${t(`table.fieldTable.major.asset.a2`)}`,
+      ],
+      lingkungan: `${t(`table.fieldTable.major.lingkungan`)}`,
+      reputasi: [
+        `${t(`table.fieldTable.major.reputasi.r1`)}`,
+        `${t(`table.fieldTable.major.reputasi.r2`)}`,
+        `${t(`table.fieldTable.major.reputasi.r3`)}`,
+      ],
+      securityFisik: `${t(`table.fieldTable.major.securityFisik`)}`,
+      securitySiber: `${t(`table.fieldTable.major.securitySiber`)}`,
+    },
+    {
+      kategoriIncident: "Catastrophic",
+      manusia: `${t(`table.fieldTable.Catastrophic.manusia`)}`,
+      asset: [
+        `${t(`table.fieldTable.Catastrophic.asset.a1`)}`,
+        `${t(`table.fieldTable.Catastrophic.asset.a2`)}`,
+      ],
+      lingkungan: `${t(`table.fieldTable.Catastrophic.lingkungan`)}`,
+      reputasi: [
+        `${t(`table.fieldTable.Catastrophic.reputasi.r1`)}`,
+        `${t(`table.fieldTable.Catastrophic.reputasi.r2`)}`,
+      ],
+      securityFisik: `${t(`table.fieldTable.Catastrophic.securityFisik`)}`,
+      securitySiber: `${t(`table.fieldTable.Catastrophic.securitySiber`)}`,
+    },
+  ];
+
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   return (
     <div className="bg-white shadow-md overflow-hidden border-t-2 border-slate-400 px-2 pb-2 pt-1">
       <div className="flex justify-between items-center">
         <h1 className="font-montserrat font-medium text-base lg:text-lg capitalize">
-          {t(`table`)}
+          {t(`table.header`)}
         </h1>
         <button
           onClick={() => setIsOpen(!isOpen)}
@@ -118,12 +111,12 @@ const TableMatrixIncidentReportComponent: React.FC<
             <thead>
               <tr>
                 <th></th>
-                <th>Manusia</th>
-                <th>Asset</th>
-                <th>Lingkungan</th>
-                <th>Reputasi</th>
-                <th>Security Fisik</th>
-                <th>Security Siber</th>
+                <th className="capitalize">{t(`table.headerColumn.m`)}</th>
+                <th className="capitalize">{t(`table.headerColumn.a`)}</th>
+                <th className="capitalize">{t(`table.headerColumn.l`)}</th>
+                <th className="capitalize">{t(`table.headerColumn.r`)}</th>
+                <th className="capitalize">{t(`table.headerColumn.sf`)}</th>
+                <th className="capitalize">{t(`table.headerColumn.ss`)}</th>
               </tr>
             </thead>
             <tbody>
