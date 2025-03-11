d4435faede311ef8874830856c534d2143842958
Helmi <helmi220602@gmail.com>
2025-03-11 16:25:15 +0800

    add user alert page done responsive mode web

--- src/pages/User/UserAlertsPage.tsx
+++ src/pages/User/UserAlertsPage.tsx
@@ -1,7 +1,43 @@
 import HeaderComponent from "../../components/HeaderComponent";
 import iconALert from "../../assets/iconAlertUser.png";
+import ViewButtonComponent from "../../components/ViewButtonComponent";
+import { GrDocumentTime } from "react-icons/gr";
 
 const UserAlertsPage: React.FC = () => {
+  const dummyData = [
+    {
+      id: 1,
+      no_report: "li200-567-700",
+      pelapor: "Helmi",
+      department: "Information Technology",
+      class_incident: "Minor",
+      time: "12 Maret 2025 - 16.30 WITA",
+    },
+    {
+      id: 2,
+      no_report: "li200-568-701",
+      pelapor: "Nurliyan",
+      department: "Human Resources",
+      class_incident: "Major",
+      time: "11 Maret 2025 - 12.30 WITA",
+    },
+    {
+      id: 3,
+      no_report: "li200-569-702",
+      pelapor: "Ahmad",
+      department: "Finance",
+      class_incident: "Critical",
+      time: "28 Februari 2025 - 15.00 WITA",
+    },
+    {
+      id: 4,
+      no_report: "li200-569-703",
+      pelapor: "Heri Muskianto",
+      department: "Operation",
+      class_incident: "Minor",
+      time: "22 Juni 2025 - 10.23 WITA",
+    },
+  ];
   return (
     <>
       {/* header */}
@@ -11,7 +47,7 @@ const UserAlertsPage: React.FC = () => {
         routeTwo="user alert"
       />
 
-      <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center gap-8 mb-4">
         {/* card notif */}
         <div className="relative flex flex-col gap-2 bg-white shadow-lg shadow-gray-200 drop-shadow-md rounded-2xl px-6 pb-6 pt-12 justify-center items-center">
           <img
@@ -34,9 +70,67 @@ const UserAlertsPage: React.FC = () => {
           </div>
         </div>
 
-        {/* card list alert */}
-        <div>
-          
+        {/* list alert */}
+        <div className=" w-full flex flex-row">
+          <div className="w-full lg:grid lg:grid-rows-1 lg:space-y-10 ">
+            {dummyData.map((item) => (
+              <div
+                key={item.id}
+                className="w-full flex flex-col gap-4 lg:flex lg:flex-row lg:justify-between lg:items-center"
+              >
+                {/* date time */}
+                <div className=" flex items-center gap-2">
+                  <GrDocumentTime className="text-xl text-black" />
+                  <div className=" whitespace-nowrap text-gray-500 tracking-wide text-base">
+                    {item.time}
+                  </div>
+                </div>
+
+                <div className="w-full h-[1px] bg-green-400 mx-3"></div>
+
+                {/* data */}
+                <div className="w-full flex flex-row items-center">
+                  {/* no Report */}
+                  <div className="flex flex-row items-center bg-green-500 py-2 gap-2 whitespace-nowrap px-4">
+                    <h1 className="text-xl font-semibold text-white font-montserrat">
+                      #
+                    </h1>
+                    <h2 className="text-white font-montserrat font-medium">
+                      {item.no_report}
+                    </h2>
+                  </div>
+                  <div className="p-3  border border-green-300 w-full flex flex-row items-center gap-8">
+                    {/* Pelapor */}
+                    <div className="whitespace-nowrap">
+                      <h1 className="text-gray-500 text-sm capitalize">
+                        Pelapor
+                      </h1>
+                      <h2>{item.pelapor}</h2>
+                    </div>
+                    {/* Departement */}
+                    <div className="whitespace-nowrap">
+                      <h1 className="text-gray-500 text-sm capitalize">
+                        Departement
+                      </h1>
+                      <h2>{item.department}</h2>
+                    </div>
+                    {/* class incident */}
+                    <div className="whitespace-nowrap">
+                      <h1 className="text-gray-500 text-sm capitalize">
+                        Classfication Incident
+                      </h1>
+                      <h2>{item.class_incident}</h2>
+                    </div>
+                    {/* detail */}
+                    <ViewButtonComponent
+                      title="detail"
+                      link="detail_incident_report"
+                    />
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </>
